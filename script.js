31f597043ca3f3b614b5d9d1127a9bac16194f3a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-06 23:44:40 +0000

    added algorithm

---
+++ script.js
@@ -0,0 +1,13 @@
+const fibonacciGenerator = (n) => {
+	if (n == 1) return [0];
+	else if (n == 2) return [0, 1];
+	else if (n < 1) return `Enter a valid value`;
+
+	let res = [0, 1];
+	for (let i = 0; i < n - 2; i++) {
+		res.push(res[i] + res[i + 1]);
+	}
+	return res;
+};
+
+fibonacciGenerator(5);
